2016f31a9214b22fbe9e06e745fa6512976bf250
Matheus Chimendes <chimendes2013@outlook.com>
2025-07-02 12:31:32 -0300

    “projeto”

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Header from "../_components/header";
 
